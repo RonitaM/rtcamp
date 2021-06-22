d8f4127c7a4ed1d4c4b6895177d7c7e81f70e2bd
Ronita Mitra <86037961+RonitaM@users.noreply.github.com>
2021-06-22 23:31:03 +0530

    Update unsub.php

--- unsub.php
+++ unsub.php
@@ -11,7 +11,7 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql="delete from users where email=$email";
+$sql="delete from users where email='$email'";
 echo $sql;
 if($conn->query($sql)===TRUE)
 {
