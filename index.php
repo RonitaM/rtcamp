740de4147f76f8dbe6757b64cf86862c9af3a8b1
Ronita Mitra <86037961+RonitaM@users.noreply.github.com>
2021-07-09 23:26:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -22,8 +22,8 @@ session_start();
 
 <?php
 
-//$conn = new mysqli('remotemysql.com', 'qFyAF6z2HL', 'ZhD8X1loVr', 'qFyAF6z2HL');
-$conn = new mysqli('localhost', 'root', '', 'rtcamp');
+$conn = new mysqli('remotemysql.com', 'qFyAF6z2HL', 'ZhD8X1loVr', 'qFyAF6z2HL');
+//$conn = new mysqli('localhost', 'root', '', 'rtcamp');
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
